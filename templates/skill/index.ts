6e78b7ac3f5d85035fe82cc053f01e244a056dee
Gokul S <gokulsunil036@gmail.com>
2026-05-22 20:59:35 +0530

    template created for skills

---
+++ templates/skill/index.ts
@@ -0,0 +1,9 @@
+import execute from './execute.js';
+
+export default {
+  name: 'TemplateSkill',
+  category: 'shared',
+  description: 'Boilerplate description for the Template Skill',
+  version: '1.0.0',
+  execute
+};
